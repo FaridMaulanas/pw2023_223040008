086b27a294a3d365821952c378738b26d65b4501
faridmaulanas <fmaulana236@gmail.com>
2023-02-19 14:31:29 +0700

    mengerjakan tugas 1

---
+++ kuliah/tugas/tugas1/1b.php
@@ -0,0 +1,24 @@
+<?php
+$nomor = '8';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<p><?php echo 'Aku adalah angka ' . '<b>' . $nomor . '</b>'; ?></p>
+<p><?php echo 'Jika aku dikali 5, maka aku sekarang menjadi ' . '<b>' . $nomor * 5 . '</b>'; ?></p>
+<p><?php echo 'Jika aku dibagi 2, maka aku sekarang menjadi ' . '<b>' . 40 / 2 . '</b>'; ?></p>
+<p><?php echo 'Jika aku ditambah 75, maka aku sekarang menjadi ' . '<b>' . 20 + 75 . '</b>'; ?></p>
+<p><?php echo 'Jika aku dikurang 20, maka aku sekarang menjadi ' . '<b>' . 95 - 20 . '</b>'; ?></p>
+
+<body>
+
+</body>
+
+</html>
---
+++ kuliah/tugas/tugas1/1c.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1C</title>
+
+    <style type="text/css">
+        .container {
+            display: flex;
+        }
+
+        .kotak {
+            width: 100px;
+            height: 100px;
+            background-color: salmon;
+            position: relative;
+            border: 1px solid #000000;
+        }
+
+        .toktok {
+            position: absolute;
+            font-family: arial;
+            font-weight: 500;
+            font-size: 30px;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="kotak">
+            <div class="toktok">1</div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="kotak">
+            <div class="toktok">2</div>
+        </div>
+        <div class="kotak">
+            <div class="toktok">2</div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="kotak">
+            <div class="toktok">3</div>
+        </div>
+        <div class="kotak">
+            <div class="toktok">3</div>
+        </div>
+        <div class="kotak">
+            <div class="toktok">3</div>
+        </div>
+    </div>
+</body>
+
+</html>
